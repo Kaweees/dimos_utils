b629c4690d66843f68ce05e66a53dfbac630e7b2
Yahas Salankimatt <yashas.salankimatt@gmail.com>
2025-06-16 19:02:40 -0700

    Added receive script and updated publish test script

--- debugging_scripts/lcm_publish_test.py
+++ debugging_scripts/lcm_publish_test.py
@@ -22,8 +22,10 @@ while True:
     msg2.y += 1
     msg2.z += 1
     lc.publish("thing1_vector3#geometry_msgs.Vector3", msg2.encode())
+    print("Vector3: x={msg2.x}, y={msg2.y}, z={msg2.z}")
     msg3.linear.x += 1
     msg3.linear.y += 1
     msg3.linear.z += 1
     lc.publish("thing1_twist#geometry_msgs.Twist", msg3.encode())
+    print("Twist: linear(x={msg3.linear.x}, y={msg3.linear.y}, z={msg3.linear.z})")
     time.sleep(.1)
